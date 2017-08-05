927a805287e39c580a9288f27a6221cf3f487520
Chase Stubblefield <chasestubblefield@gmail.com>
2017-08-05 14:11:39 -0700

    Basic parsing of git status

---
+++ src/lib.rs
@@ -0,0 +1,105 @@
+#[derive(Debug, PartialEq, Eq)]
+struct Status {
+    untracked: bool,
+    unstaged: bool,
+    staged: bool,
+    ahead: bool,
+    behind: bool,
+    branch: String,
+    upstream: String,
+}
+
+impl Status {
+    fn new(status_txt: &str) -> Result<Status, &'static str> {
+        let mut s = Status {
+            untracked: false,
+            unstaged: false,
+            staged: false,
+            ahead: false,
+            behind: false,
+            branch: String::new(),
+            upstream: String::new(),
+        };
+        for line in status_txt.lines() {
+            if line.starts_with("?") {
+                s.untracked = true;
+            }
+            if line.starts_with("1 .M") {
+                s.unstaged = true;
+            }
+            if line.starts_with("1 M.") {
+                s.staged = true;
+            }
+            if line.starts_with("# branch.head") {
+                let v: Vec<&str> = line.split(' ').collect();
+                s.branch = String::from(v[2]);
+            }
+            if line.starts_with("# branch.upstream") {
+                let v: Vec<&str> = line.split(' ').collect();
+                s.upstream = String::from(v[2]);
+            }
+            if line.starts_with("# branch.ab") {
+                let v: Vec<&str> = line.split(' ').collect();
+                if v[2] != "+0" {
+                    s.ahead = true;
+                }
+                if v[3] != "-0" {
+                    s.behind = true;
+                }
+            }
+        }
+        Ok(s)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn parse_dirty() {
+        let test_status = "\
+# branch.oid 3845e7a3c3aadaaebb2d1b261bf07a9357d35a79
+# branch.head master
+# branch.upstream origin/master
+# branch.ab +1 -1
+1 M. N... 100644 100644 100644 1290f45e7ad7575848a436d8febbd6c4ba07f1f3 311c77295c5b6056f4599c2b8d0a019d4c76746a README.md
+1 .M N... 100644 100644 100644 5e8a8090976077ddf16252a560460a20dbbdd6a5 5e8a8090976077ddf16252a560460a20dbbdd6a5 gh-pages.sh
+? foo.txt
+";
+        assert_eq!(
+            Status::new(&test_status).unwrap(),
+            Status {
+                untracked: true,
+                unstaged: true,
+                staged: true,
+                branch: String::from("master"),
+                upstream: String::from("origin/master"),
+                ahead: true,
+                behind: true,
+            }
+        );
+    }
+
+    #[test]
+    fn parse_clean() {
+        let test_status = "\
+# branch.oid 3845e7a3c3aadaaebb2d1b261bf07a9357d35a79
+# branch.head master
+# branch.upstream origin/master
+# branch.ab +0 -0
+";
+        assert_eq!(
+            Status::new(&test_status).unwrap(),
+            Status {
+                untracked: false,
+                unstaged: false,
+                staged: false,
+                branch: String::from("master"),
+                upstream: String::from("origin/master"),
+                ahead: false,
+                behind: false,
+            }
+        );
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::process::Command;
+use std::str;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn run() {
+    let output = Command::new("/usr/bin/git")
+    .args(&["status", "--porcelain=2", "-b"])
+    .output()
+    .expect("failed to execute process");
+    if output.status.success() {
+        let output = match str::from_utf8(&output.stdout) {
+            Ok(v) => v,
+            Err(e) => panic!("Invalid UTF-8: {}", e),
+        };
+        let lines: Vec<&str> = output.lines().collect();
+        println!("{:?}", lines);
+    }
+}
