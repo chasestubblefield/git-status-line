aa5ed9d291ac46d91f46835c1c97cd43f1f49c5b
Chase Stubblefield <chasestubblefield@gmail.com>
2017-08-07 15:16:49 -0700

    Refactor to allow optional branch and upstream info

--- src/lib.rs
+++ src/lib.rs
@@ -1,56 +1,54 @@
 #[derive(Debug, PartialEq, Eq)]
 struct GitStatus {
-    object_id: String,
-    branch: String,
-    upstream: String,
-    ahead: bool,
-    behind: bool,
+    branch: Option<BranchInfo>,
+    upstream: Option<UpstreamInfo>,
     staged: bool,
     unstaged: bool,
     untracked: bool,
+    ignored: bool,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct BranchInfo {
+    object_id: String,
+    branch: Option<String>,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+struct UpstreamInfo {
+    upstream: String,
+    ahead: bool,
+    behind: bool,
 }
 
 impl GitStatus {
     fn new(status_txt: &str) -> Result<GitStatus, &'static str> {
         let mut s = GitStatus {
-            object_id: String::new(),
-            branch: String::new(),
-            upstream: String::new(),
-            ahead: false,
-            behind: false,
+            branch: None,
+            upstream: None,
             staged: false,
             unstaged: false,
             untracked: false,
+            ignored: false,
         };
         for line in status_txt.lines() {
-            if line.starts_with("# branch.oid") {
-                let v: Vec<&str> = line.split(' ').collect();
-                s.object_id = String::from(v[2]);
-            } else if line.starts_with("# branch.head") {
-                let v: Vec<&str> = line.split(' ').collect();
-                s.branch = String::from(v[2]);
-                continue;
-            } else if line.starts_with("# branch.upstream") {
-                let v: Vec<&str> = line.split(' ').collect();
-                s.upstream = String::from(v[2]);
-                continue;
-            } else if line.starts_with("# branch.ab") {
-                let v: Vec<&str> = line.split(' ').collect();
-                if v[2] != "+0" {
-                    s.ahead = true;
+            let words: Vec<&str> = line.split(' ').collect();
+            if let Some(&first) = words.first() {
+                match first {
+                    "#" => {
+                    },
+                    "1" => {
+                        s.staged = true;
+                        s.unstaged = true;
+                    },
+                    "?" => {
+                        s.untracked = true;
+                    },
+                    "!" => {
+                        s.ignored = true;
+                    },
+                    _ => {},
                 }
-                if v[3] != "-0" {
-                    s.behind = true;
-                }
-            } else if line.starts_with("1 M.") {
-                s.staged = true;
-            } else if line.starts_with("1 .M") {
-                s.unstaged = true;
-            } else if line.starts_with("1 MM") {
-                s.unstaged = true;
-                s.staged = true;
-            } else if line.starts_with("?") {
-                s.untracked = true;
             }
         }
         Ok(s)
@@ -62,6 +60,22 @@ mod tests {
     use super::*;
 
     #[test]
+    fn parse_clean() {
+        let test_status = "\n";
+        assert_eq!(
+            GitStatus::new(test_status).unwrap(),
+            GitStatus {
+                branch: None,
+                upstream: None,
+                staged: false,
+                unstaged: false,
+                untracked: false,
+                ignored: false,
+            }
+        );
+    }
+
+    #[test]
     fn parse_dirty() {
         let test_status = "\
 # branch.oid 3845e7a3c3aadaaebb2d1b261bf07a9357d35a79
@@ -71,66 +85,81 @@ mod tests {
 1 M. N... 100644 100644 100644 1290f45e7ad7575848a436d8febbd6c4ba07f1f3 311c77295c5b6056f4599c2b8d0a019d4c76746a README.md
 1 .M N... 100644 100644 100644 5e8a8090976077ddf16252a560460a20dbbdd6a5 5e8a8090976077ddf16252a560460a20dbbdd6a5 gh-pages.sh
 ? foo.txt
+! ignored.txt
 ";
         assert_eq!(
-            GitStatus::new(&test_status).unwrap(),
+            GitStatus::new(test_status).unwrap(),
             GitStatus {
-                object_id: String::from("3845e7a3c3aadaaebb2d1b261bf07a9357d35a79"),
-                branch: String::from("master"),
-                upstream: String::from("origin/master"),
-                ahead: true,
-                behind: true,
+                branch: Some(BranchInfo {
+                    object_id: String::from("3845e7a3c3aadaaebb2d1b261bf07a9357d35a79"),
+                    branch: Some(String::from("master")),
+                }),
+                upstream: Some(UpstreamInfo {
+                    upstream: String::from("origin/master"),
+                    ahead: true,
+                    behind: true,
+                }),
                 staged: true,
                 unstaged: true,
                 untracked: true,
+                ignored: true,
             }
         );
     }
 
     #[test]
-    fn parse_clean() {
+    fn parse_branch() {
         let test_status = "\
 # branch.oid 3845e7a3c3aadaaebb2d1b261bf07a9357d35a79
 # branch.head master
-# branch.upstream origin/master
-# branch.ab +0 -0
 ";
         assert_eq!(
-            GitStatus::new(&test_status).unwrap(),
-            GitStatus {
+            GitStatus::new(test_status).unwrap().branch,
+            Some(BranchInfo {
                 object_id: String::from("3845e7a3c3aadaaebb2d1b261bf07a9357d35a79"),
-                branch: String::from("master"),
-                upstream: String::from("origin/master"),
-                ahead: false,
-                behind: false,
-                staged: false,
-                unstaged: false,
-                untracked: false,
-            }
+                branch: Some(String::from("master")),
+            })
         );
     }
 
     #[test]
-    fn parse_staged_and_unstaged_changes_on_same_file() {
+    fn parse_upstream() {
         let test_status = "\
-# branch.oid 3845e7a3c3aadaaebb2d1b261bf07a9357d35a79
-# branch.head master
 # branch.upstream origin/master
-# branch.ab +0 -0
+# branch.ab +1 -1
+";
+        assert_eq!(
+            GitStatus::new(test_status).unwrap().upstream,
+            Some(UpstreamInfo {
+                upstream: String::from("origin/master"),
+                ahead: true,
+                behind: true,
+            })
+        );
+    }
+
+    #[test]
+    fn parse_staged_and_unstaged_on_same_file() {
+        let test_status = "\
 1 MM N... 100644 100644 100644 1290f45e7ad7575848a436d8febbd6c4ba07f1f3 311c77295c5b6056f4599c2b8d0a019d4c76746a README.md
 ";
+        let s = GitStatus::new(test_status).unwrap();
+        assert!(s.staged);
+        assert!(s.unstaged);
+    }
+
+    #[test]
+    fn parse_detached_head() {
+        let test_status = "\
+# branch.oid 2a7bb916bba69a5fc9d428acc80b1bce64e3e0bc
+# branch.head (detached)
+";
         assert_eq!(
-            GitStatus::new(&test_status).unwrap(),
-            GitStatus {
-                object_id: String::from("3845e7a3c3aadaaebb2d1b261bf07a9357d35a79"),
-                branch: String::from("master"),
-                upstream: String::from("origin/master"),
-                ahead: false,
-                behind: false,
-                staged: true,
-                unstaged: true,
-                untracked: false,
-            }
+            GitStatus::new(test_status).unwrap().branch,
+            Some(BranchInfo {
+                object_id: String::from("2a7bb916bba69a5fc9d428acc80b1bce64e3e0bc"),
+                branch: None
+            })
         );
     }
 }
