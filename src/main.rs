ea71b2a1f51b387b3c18d9a2fdd2419a5e449f6e
Chase Stubblefield <chasestubblefield@gmail.com>
2017-08-15 16:41:39 -0700

    Separate external call into a function

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,22 @@
-extern crate git_status_line;
-
+use std::process;
 use std::process::Command;
-use std::str;
 
+extern crate git_status_line;
 use git_status_line::Status;
 
 fn main() {
-    let output = Command::new("git")
-        .args(&["status", "--porcelain=2", "--branch"])
-        .output()
-        .expect("failed to execute process");
-    if output.status.success() {
-        let output = match str::from_utf8(&output.stdout) {
-            Ok(v) => v,
-            Err(e) => panic!("Invalid UTF-8: {}", e),
-        };
-        let status = match Status::new(output) {
-            Ok(v) => v,
-            Err(e) => panic!("{}", e),
-        };
-        print!("{} ", status);
+    let (status_txt, success) = capture_stdout("git", &["status", "--porcelain=2", "--branch"]);
+    if !success {
+        process::exit(1);
     }
+    let status = Status::new(&status_txt).unwrap();
+    println!("{}", status);
+}
+
+fn capture_stdout(prog: &str, args: &[&str]) -> (String, bool) {
+    let result = Command::new(prog)
+        .args(args)
+        .output()
+        .unwrap();
+    (String::from_utf8(result.stdout).unwrap(), result.status.success())
 }
